1fc443240a8e6eec118dc6f8425ed877838ab930
Buissartt <toinou@live.fr>
2020-11-29 20:55:55 +0100

    [Change] Switch from vanilla JS to JQuery for the draggable element.

--- public/js/Drag.js
+++ public/js/Drag.js
@@ -1,35 +1,10 @@
-// Make the DIV element draggable:
-dragElement(document.getElementById("draggablediv"));
-
-function dragElement(elmnt) {
-  var start = 0, end = 0;
-  elmnt.onmousedown = dragMouseDown;
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    end = e.clientX;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
+$( "#bullet" ).draggable({
+  axis: "x",
+  containment: "#draggablediv",
+  cursor: "move",
+  //helper: "clone", //Permet de cloner l'objet et de bouger seulement le clone
+  drag: function( event, ui ) {
+    console.log(ui.position.left);
   }
+});
 
-  function elementDrag(e) {
-    var position = document.getElementById("draggablediv").getBoundingClientRect();
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    console.log(e.clientX);
-    start = (position.left < 0) ? -1 : (end - e.clientX);
-    end = e.clientX;
-    // set the element's new position:
-    elmnt.style.left = (elmnt.offsetLeft - start) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
