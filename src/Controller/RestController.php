b5e76411541d1095371f53e392cb7adb02aa034e
MrAnyx <1ptitigre@gmail.com>
2020-12-04 05:14:24 +0100

    Fix: Fixed style map

--- src/Controller/RestController.php
+++ src/Controller/RestController.php
@@ -11,17 +11,19 @@ use Symfony\Component\HttpClient\HttpClient;
 class RestController extends AbstractController
 {
     /**
-     * @Route("/rest/polution/get/{url}", name="rest")
+     * @Route("/rest/pollution/get/{routeName}", name="rest")
      */
-    public function index(String $url): Response
+    public function index(String $routeName): Response
     {
 
         //get time stamp before 
         $curTime = microtime(true);
 
+        $url = "http://51.210.122.194".$this->generateUrl($routeName);
+
         // make request
         $client = HttpClient::create();
-        $requestResponse = $client->request('GET', urldecode($url));
+        $requestResponse = $client->request('GET', $url);
         $requestResponse->getContent();
         //get time stamp after
         $timeConsumed = round(microtime(true) - $curTime,3)*1000;  
